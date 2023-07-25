77c9db3129c8ce7edf4f15a3189720cddc8d6b8f
Afeh <balogunafebu22@gmail.com>
2023-07-26 00:07:08 +0100

    moved _puts to new file

--- string2.c
+++ string2.c
@@ -18,6 +18,36 @@ void print_str_list(char **str)
 	}
 }
 
+/**
+ * _puts: outputs a null terminated string to the standard output
+ *
+ * @str: pointer the the null terminated string to be printed
+ * Return: the number of characters written
+ */
+
+int _puts(const char *str)
+{
+	int num_chars_written;
+
+	if (str == NULL)
+		return (EOF);
+
+	num_chars_written = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		num_chars_written++;
+	}
+
+	_putchar('\n');
+
+	num_chars_written++;
+
+
+	return (num_chars_written);
+}
 
 
 int _putchar(char c)
