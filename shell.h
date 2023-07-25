cbe0a0b9e9b174e4fb8117b35bc0a2b54a55d5d6
Ade-nike17 <perfectnyke@gmail.com>
2023-07-25 07:23:22 -0700

    adds more prototypes

--- shell.h
+++ shell.h
@@ -10,11 +10,17 @@
 
 extern char **environ;
 
+
 /* prototype functions */
 char **split_input_line(char *lineptr, const char *delim);
 char **get_input_tokens();
-void execmd(char **tokens);
+void execmd(char **tokens, char **env);
 char *find_path(char *cmd);
+void exit_shell(void);
+void print_env(char **env);
+void print_str_list(char **list);
+char **dup_env();
+void free_env(char **environ);
 size_t strlen(const char *str);
 char *strcpy(char *dest, const char *src);
 char *strcat(char *dest, const char *src);
