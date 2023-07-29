213103bf18dff340403d5b9ad3cebe2e38410253
Ade-nike17 <perfectnyke@gmail.com>
2023-07-29 06:21:26 -0700

    set a glag to monitor while loop

--- shell.c
+++ shell.c
@@ -13,10 +13,11 @@ int main(__attribute__((unused)) int argc, __attribute__((unused))char **argv)
 	char **tokens;
 	char **env_cp = dup_env();
 	int i;
+	int loop = 1;
 
-	while (1)
+	while (loop)
 	{
-		write(STDIN_FILENO, "$ ", 3);
+		write(STDOUT_FILENO, "$ ", 2); 
 		tokens = get_input_tokens();
 		if (tokens == NULL)
 		{
@@ -28,19 +29,14 @@ int main(__attribute__((unused)) int argc, __attribute__((unused))char **argv)
 			if (should_exit(tokens[0]))
 			{
 				handle_exit(tokens, env_cp);
+				loop = 0;
 			}
 			if (is_env_cmd(tokens[0]))
 			{
 				handle_env(tokens, env_cp);
 				continue;
 			}
-			/* Remove the prompt before executing the command */
-			if (fork() == 0)
-			{
-				execmd(tokens);
-				exit(EXIT_SUCCESS);
-			}
-			wait(NULL);
+			execmd(tokens);
 		}
 		/* Free the allocated memory for token */
 		for (i = 0; tokens[i] != NULL; i++)
